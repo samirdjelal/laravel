1fd2c9c0b51c3c937a666b0388104770bb0e9f2e
Samir Djelal <samir.djelal.webdesign@gmail.com>
2022-05-05 23:56:30 +0100

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
-pub struct Deployer {}
+extern crate yaml_rust;
+
+use yaml_rust::{YamlLoader, Yaml};
+
+pub struct Deployer {
+	config: Vec<Yaml>,
+}
 
 impl Deployer {
 	pub fn new() -> Self {
-		Self {}
+		Self { config: vec![] }
+	}
+	pub fn configure(&mut self, path: &str) {
+		if !std::path::Path::new(path).exists() {
+			println!("File does not exist: {}", path);
+			std::process::exit(1);
+		}
+		let contents = std::fs::read_to_string(path).expect("Failed to read config file");
+		let docs = YamlLoader::load_from_str(contents.as_str()).unwrap();
+		self.config = docs.clone();
+	}
+	pub fn deploy(&self) {
+		for doc in self.config.iter() {
+			let mut yaml = doc.clone();
+			
+			match yaml.as_hash() {
+				Some(hash) => {
+					for (key, value) in hash.iter() {
+						println!("{:?}: {:?}", key, value);
+					}
+				}
+				None => {
+					println!("{:?}", yaml);
+				}
+			}
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::env::args;
-use std::process::exit;
 use deployer::Deployer;
 
 fn main() {
-    println!("Hello, world!");
-	let config_file = args().skip(1).next();
-	if config_file.is_none() {
+	let attr = std::env::args().skip(1).next();
+	if attr.is_none() {
 		eprintln!("No config file specified");
-		exit(1);
+		std::process::exit(1);
 	}
-	println!("{}", config_file.unwrap());
-	// Deployer::new();
+	let mut deployer = Deployer::new();
+	deployer.configure(attr.unwrap().as_str());
+	deployer.deploy();
 }
